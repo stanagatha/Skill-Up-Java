e966c341edf86ad3d2b77c3cbe5f35c19ee771e0
Natalia <gamarranatalia8@gmail.com>
2022-11-01 18:25:45 -0300

    add entity transactions

--- src/main/java/org/alkemy/wallet/model/Transaction.java
+++ src/main/java/org/alkemy/wallet/model/Transaction.java
@@ -1,2 +1,34 @@
-package org.alkemy.wallet.model;public class Transaction {
+package org.alkemy.wallet.model;
+import com.sun.istack.NotNull;
+import com.sun.istack.Nullable;
+import lombok.Data;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    private double amount;
+    @NotNull
+    private String type_transaction;
+    @Nullable
+    private String descrip;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date transactionDate;
+
+    public Transaction() {
+    }
+
+    public Transaction(long id, double amount, String type_transaction, String descrip, Date transactionDate) {
+        this.id = id;
+        this.amount = amount;
+        this.type_transaction = type_transaction;
+        this.descrip = descrip;
+        this.transactionDate = transactionDate;
+    }
 }
