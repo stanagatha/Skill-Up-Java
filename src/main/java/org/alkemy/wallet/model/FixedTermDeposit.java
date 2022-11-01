1871e90a2f0be205f8ce744820939e2ab8684a43
no7sag <81098953+no7sag@users.noreply.github.com>
2022-11-01 19:41:58 -0300

    AL-5: Create FixedTermDeposit model and repository

---
+++ src/main/java/org/alkemy/wallet/model/FixedTermDeposit.java
@@ -0,0 +1,41 @@
+package org.alkemy.wallet.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Getter
+@Setter
+@Table(name = "FIXED_TERM_DEPOSITS")  // mayus
+public class FixedTermDeposit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "FIXED_TERM_DEPOSIT_ID")
+    private Long fixedTermDepositsId;
+
+    @Column(name = "AMOUNT", nullable = false)
+    private Double amount;
+
+    @ManyToOne
+    @JoinColumn(name = "ACCOUNT_ID")
+    private Account accountId;
+
+    @Column(name = "INTEREST", nullable = false)
+    private Double interest;
+
+    @JsonFormat(pattern = "yyyy/MM/dd")
+    @Column(name = "CREATION_DATE")
+    private Date creationDate;
+
+    @JsonFormat(pattern = "yyyy/MM/dd")
+    @Column(name = "CLOSING_DATE")
+    private Date closingDate;
+
+}
