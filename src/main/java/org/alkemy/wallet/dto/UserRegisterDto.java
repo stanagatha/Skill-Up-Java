1d7aef98bbb9650d09e8133a2a664c004247e946
Ignacio Avila <ignacioavilamdp@gmail.com>
2022-11-03 11:02:59 -0300

    AL-1: Create User model and repository. (#17)
    * AL-1: added User entity.
    
    * AL-1: added User basic dto.
    
    * AL-1: added user register dto.
    
    * AL-1: added User mapper.
    
    * AL-1: added User repository.

---
+++ src/main/java/org/alkemy/wallet/dto/UserRegisterDto.java
@@ -0,0 +1,9 @@
+package org.alkemy.wallet.dto;
+
+public class UserRegisterDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
