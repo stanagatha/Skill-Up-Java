746d042f2844ff4417a59ce4a1217fd7b4c4c5a0
Ignacio Avila <ignacioavilamdp@gmail.com>
2022-11-03 10:57:51 -0300

    AL-1: added User repository.

---
+++ src/main/java/org/alkemy/wallet/repository/IUserRepository.java
@@ -0,0 +1,7 @@
+package org.alkemy.wallet.repository;
+
+import org.alkemy.wallet.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+}
