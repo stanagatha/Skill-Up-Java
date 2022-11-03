6efdfde7f2c34fcb01168b6602710806ec34cfb8
Ignacio Avila <ignacioavilamdp@gmail.com>
2022-11-02 21:03:27 -0300

    AL-9: ExceptionHandler modified to support CustomException

--- src/main/java/org/alkemy/wallet/exception/ExceptionHandler.java
+++ src/main/java/org/alkemy/wallet/exception/ExceptionHandler.java
@@ -1,7 +1,20 @@
 package org.alkemy.wallet.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler({CustomException.class})
+    @ResponseBody
+    public ResponseEntity<String> badRequestExceptionHandler(HttpServletRequest req, RuntimeException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
 }
