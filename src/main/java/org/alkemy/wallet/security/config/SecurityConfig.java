b4a644b7d46243012c9f7a5b61ce512668aa1b8a
Ignacio Avila <ignacioavilamdp@gmail.com>
2022-11-03 13:32:35 -0300

    CompilationFix : modify SecurityConfig to allow testing without credentials.

--- src/main/java/org/alkemy/wallet/security/config/SecurityConfig.java
+++ src/main/java/org/alkemy/wallet/security/config/SecurityConfig.java
@@ -1,7 +1,27 @@
 package org.alkemy.wallet.security.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/*
+ * This class is just to allow some testing.
+ * All requests are permitted, without exception.
+ *
+ */
+
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().permitAll();
+    }
 }
