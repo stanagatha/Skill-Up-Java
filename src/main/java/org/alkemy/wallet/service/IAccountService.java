a2e764c6a618a24680ef82ba1a8dcb925c9e6622
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-11-04 14:28:49 -0300

    AL-11: Method added in IAccountService for find accounts by user

--- src/main/java/org/alkemy/wallet/service/IAccountService.java
+++ src/main/java/org/alkemy/wallet/service/IAccountService.java
@@ -1,11 +1,16 @@
 package org.alkemy.wallet.service;
 
+import org.alkemy.wallet.dto.AccountDto;
 import org.alkemy.wallet.model.Account;
+
+import java.util.List;
 
 public interface IAccountService {
 
     public void saveAccount(Account account);
 
     public Account findById(long accountId);
+
+    List<AccountDto> findAllByUser(Long userId);
 
 }
