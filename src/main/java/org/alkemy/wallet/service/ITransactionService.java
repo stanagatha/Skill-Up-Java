62f11207fd6cc6d0fbdac05eda4b2d1c29514255
julianDGS <jdjuliangs@gmail.com>
2022-11-04 10:52:13 -0500

    AL-17 mapper moved from controller to service

--- src/main/java/org/alkemy/wallet/service/ITransactionService.java
+++ src/main/java/org/alkemy/wallet/service/ITransactionService.java
@@ -1,9 +1,10 @@
 package org.alkemy.wallet.service;
 
+import org.alkemy.wallet.dto.TransactionDto;
 import org.alkemy.wallet.model.Transaction;
 
 public interface ITransactionService {
 
-    public Transaction save(Transaction transaction);
+    public TransactionDto save(TransactionDto transaction);
 
 }
