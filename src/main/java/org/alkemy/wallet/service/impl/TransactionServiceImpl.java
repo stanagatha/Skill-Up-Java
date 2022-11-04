3e5acf86a6018811bd889ef01e98157b1384d6c8
julianDGS <114264718+julianDGS@users.noreply.github.com>
2022-11-04 12:59:20 -0500

    AL-17 mapper moved to service (#26)
    * AL-17 mapper moved from controller to service
    
    * adjustment exception controller

--- src/main/java/org/alkemy/wallet/service/impl/TransactionServiceImpl.java
+++ src/main/java/org/alkemy/wallet/service/impl/TransactionServiceImpl.java
@@ -1,32 +1,44 @@
 package org.alkemy.wallet.service.impl;
 
+import org.alkemy.wallet.dto.TransactionDto;
+import org.alkemy.wallet.mapper.TransactionMapper;
+import org.alkemy.wallet.model.Account;
 import org.alkemy.wallet.model.Transaction;
 import org.alkemy.wallet.repository.ITransactionRepository;
+import org.alkemy.wallet.service.IAccountService;
 import org.alkemy.wallet.service.ITransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Calendar;
+import java.util.Date;
+
 @Service
 public class TransactionServiceImpl implements ITransactionService {
 
     private final ITransactionRepository transactionRepository;
+    private final TransactionMapper transactionMapper;
+    private final IAccountService accountService;
 
     @Autowired
-    public TransactionServiceImpl(ITransactionRepository transactionRepository) {
+    public TransactionServiceImpl(ITransactionRepository transactionRepository, TransactionMapper transactionMapper, IAccountService accountService) {
         this.transactionRepository = transactionRepository;
+        this.transactionMapper = transactionMapper;
+        this.accountService = accountService;
     }
 
     @Override
     @Transactional
-    public Transaction save(Transaction transaction) {
-        if(transaction.getAmount() <=0){
+    public TransactionDto save(TransactionDto transactionDto) {
+        if(transactionDto.getAmount() <=0){
             throw new IllegalArgumentException("Amount must be greater than 0");
         }
-        if(transaction.getTransactionDate() == null){
-            throw new IllegalArgumentException("Date must not be null");
-        }
-        return transactionRepository.save(transaction);
+        transactionDto.setTransactionDate(new Date(Calendar.getInstance().getTimeInMillis()));
+        Account account = accountService.findById(transactionDto.getAccountId());
+        Transaction transaction = transactionMapper.transactionDtoToTransaction(transactionDto);
+        transaction.setAccountId(account);
+        return transactionMapper.transactionToTransactionDto(transactionRepository.save(transaction));
     }
 
 }
