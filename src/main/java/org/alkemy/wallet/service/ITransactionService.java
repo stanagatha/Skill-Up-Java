4d8d1ccdb87f66a3f2d2f90db322a9accfc89c96
William Fonseca <santifon15@gmail.com>
2022-11-05 16:25:11 -0500

    add function to list transactions by user

--- src/main/java/org/alkemy/wallet/service/ITransactionService.java
+++ src/main/java/org/alkemy/wallet/service/ITransactionService.java
@@ -3,11 +3,10 @@ package org.alkemy.wallet.service;
 import java.util.List;
 
 import org.alkemy.wallet.dto.TransactionDto;
-import org.alkemy.wallet.model.Transaction;
 
 public interface ITransactionService {
 
     public TransactionDto save(TransactionDto transaction);
 
-    public TransactionDto getAll(long userId);
+    public List<TransactionDto> getAll(long userId);
 }
--- src/main/java/org/alkemy/wallet/service/impl/TransactionServiceImpl.java
+++ src/main/java/org/alkemy/wallet/service/impl/TransactionServiceImpl.java
@@ -2,39 +2,33 @@ package org.alkemy.wallet.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import org.alkemy.wallet.dto.TransactionDto;
 import org.alkemy.wallet.dto.TransactionDto;
 import org.alkemy.wallet.mapper.TransactionMapper;
 import org.alkemy.wallet.model.Account;
 import org.alkemy.wallet.model.Transaction;
-import org.alkemy.wallet.model.User;
 import org.alkemy.wallet.repository.IAccountRepository;
 import org.alkemy.wallet.repository.ITransactionRepository;
 import org.alkemy.wallet.repository.IUserRepository;
 import org.alkemy.wallet.service.IAccountService;
 import org.alkemy.wallet.service.ITransactionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 
 import java.util.Calendar;
 import java.util.Date;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionServiceImpl implements ITransactionService {
 
     private final ITransactionRepository transactionRepository;
     private final TransactionMapper transactionMapper;
     private final IAccountService accountService;
-
-    @Autowired
-    public TransactionServiceImpl(ITransactionRepository transactionRepository, TransactionMapper transactionMapper, IAccountService accountService) {
-        this.transactionRepository = transactionRepository;
-        this.transactionMapper = transactionMapper;
-        this.accountService = accountService;
-    }
+    private final IUserRepository userRepository;
+    private final IAccountRepository accountRepository;
 
     @Override
     @Transactional
@@ -50,18 +44,15 @@ public class TransactionServiceImpl implements ITransactionService {
     }
 
     @Override
-    public TransactionDto getAll(long userId) {
-        return (TransactionDto) null /* userRepository.findById(userId).map(user -> {
+    public List<TransactionDto> getAll(long userId) {
+        return userRepository.findById(userId).map(user -> {
             List<Account> accounts= accountRepository.findAllByUser(user);
             List<Transaction> transactions= new ArrayList<>();
             accounts.forEach(acc -> {
                 transactions.addAll(transactionRepository.findAllByAccountId(acc));
             });
-            System.out.println("Get all, no users");
-            transactions.forEach(System.out::println);;
-            //implemente mapper;
-            return null;
-        }).orElse(null)*/;
+            return transactionMapper.toTransactionsDto(transactions);
+        }).orElse(null);
     }
 
 }
