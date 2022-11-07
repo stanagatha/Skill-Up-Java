57abec453c58c777cd86bc3c61bb546d9500cbf4
William Fonseca <santifon15@gmail.com>
2022-11-06 19:38:09 -0500

    merge develop

--- src/main/java/org/alkemy/wallet/service/ITransactionService.java
+++ src/main/java/org/alkemy/wallet/service/ITransactionService.java
@@ -3,16 +3,10 @@ package org.alkemy.wallet.service;
 import java.util.List;
 
 import org.alkemy.wallet.dto.TransactionDto;
-<<<<<<< HEAD
-=======
-import org.alkemy.wallet.dto.TransactionDto;
-import org.alkemy.wallet.model.Transaction;
->>>>>>> b4b8dbf (add getAll method)
 
 public interface ITransactionService {
 
-    public Transaction save(Transaction transaction);
-    public TransactionDto getAll(long userId);
+    public TransactionDto save(TransactionDto transaction);
 
-    public List<TransactionDto> getAllByUser(long userId);
+    public List<TransactionDto> getAll(long userId);
 }
