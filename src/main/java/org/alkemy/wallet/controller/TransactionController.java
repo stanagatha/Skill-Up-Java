4e78011a6cc343f1257b7bf6ded86a07b353b2f6
William Fonseca <santifon15@gmail.com>
2022-11-06 19:47:10 -0500

    add edit method to transactions

--- src/main/java/org/alkemy/wallet/controller/TransactionController.java
+++ src/main/java/org/alkemy/wallet/controller/TransactionController.java
@@ -24,15 +24,13 @@ public class TransactionController {
     @GetMapping("/{user_id}")
     public ResponseEntity<?> getAllByUser(@PathVariable(name = "user_id") long userId){
         try {
+            //Implement authentication and authorization.
             return new ResponseEntity<>(transactionService.getAllByUser(userId), HttpStatus.OK);
         } catch (IllegalArgumentException illegalArgumentException){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-<<<<<<< HEAD
             System.out.println("ERROR ON TRANSACTION CONTROLLER, GENERAL EXCEPTION");
             System.out.println(e.getMessage());
-=======
->>>>>>> develop
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -40,6 +38,7 @@ public class TransactionController {
     @PatchMapping("/{id}")
     public ResponseEntity<?> edit(@PathVariable(name = "id") long id, @RequestBody Map<String, String> requestBody){
         try {
+            //Implement authentication and authorization.
             return new ResponseEntity<TransactionDto>(transactionService.edit(id, id, requestBody.get("description")), HttpStatus.OK);
         } catch (IllegalArgumentException illegalArgumentException){
             System.out.println("Error IAE edit method in transaction controller:\n" + illegalArgumentException.getMessage());
--- src/main/java/org/alkemy/wallet/service/ITransactionService.java
+++ src/main/java/org/alkemy/wallet/service/ITransactionService.java
@@ -8,10 +8,6 @@ public interface ITransactionService {
 
     public TransactionDto save(TransactionDto transaction);
     public List<TransactionDto> getAllByUser(long userId);
-<<<<<<< HEAD
     public TransactionDto edit(long userId, long id, String description);
-=======
-    public TransactionDto edit(long userId, long id, String desctiption);
->>>>>>> develop
 
 }
