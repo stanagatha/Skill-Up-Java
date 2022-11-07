c2f5dc73696dd2c248822c5667dbadade984a93a
William Fonseca <santifon15@gmail.com>
2022-11-06 19:38:09 -0500

    merge develop

--- src/main/java/org/alkemy/wallet/service/ITransactionService.java
+++ src/main/java/org/alkemy/wallet/service/ITransactionService.java
@@ -3,15 +3,10 @@ package org.alkemy.wallet.service;
 import java.util.List;
 
 import org.alkemy.wallet.dto.TransactionDto;
-import org.alkemy.wallet.dto.TransactionDto;
-import org.alkemy.wallet.model.Transaction;
 
 public interface ITransactionService {
 
-    public Transaction save(Transaction transaction);
-    public TransactionDto getAll(long userId);
+    public TransactionDto save(TransactionDto transaction);
 
-    public List<TransactionDto> getAllByUser(long userId);
-    public TransactionDto edit(long userId, long id, String description);
-
+    public List<TransactionDto> getAll(long userId);
 }
--- src/main/java/org/alkemy/wallet/service/impl/TransactionServiceImpl.java
+++ src/main/java/org/alkemy/wallet/service/impl/TransactionServiceImpl.java
@@ -2,12 +2,10 @@ package org.alkemy.wallet.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.alkemy.wallet.dto.TransactionDto;
 import org.alkemy.wallet.model.Account;
 import org.alkemy.wallet.model.Transaction;
-import org.alkemy.wallet.model.User;
 import org.alkemy.wallet.repository.IAccountRepository;
 import org.alkemy.wallet.repository.ITransactionRepository;
 import org.alkemy.wallet.repository.IUserRepository;
@@ -21,36 +19,30 @@ import java.util.Calendar;
 import java.util.Date;
 
 @Service
-@RequiredArgsConstructor
 public class TransactionServiceImpl implements ITransactionService {
 
     private final ITransactionRepository transactionRepository;
-    private final IAccountRepository accountRepository;
+    private final TransactionMapper transactionMapper;
+    private final IAccountService accountService;
     private final IUserRepository userRepository;
-
-    @Autowired
-    public TransactionServiceImpl(ITransactionRepository transactionRepository, IAccountRepository accountRepository, IUserRepository userRepository) {
-        this.transactionRepository = transactionRepository;
-        this.accountRepository= accountRepository;
-        this.userRepository= userRepository;
-    }
-
-	@Override
-	@Transactional
-	public TransactionDto save(TransactionDto transactionDto) {
-		if (transactionDto.getAmount() <= 0) {
-			throw new IllegalArgumentException("Amount must be greater than 0");
-		}
-		transactionDto.setTransactionDate(new Date(Calendar.getInstance().getTimeInMillis()));
-		Account account = accountService.findById(transactionDto.getAccountId());
-		Transaction transaction = transactionMapper.transactionDtoToTransaction(transactionDto);
-		transaction.setAccountId(account);
-		return transactionMapper.transactionToTransactionDto(transactionRepository.save(transaction));
-	}
+    private final IAccountRepository accountRepository;
 
     @Override
-    public TransactionDto getAll(long userId) {
-        return (TransactionDto) userRepository.findById(userId).map(user -> {
+    @Transactional
+    public TransactionDto save(TransactionDto transactionDto) {
+        if(transactionDto.getAmount() <=0){
+            throw new IllegalArgumentException("Amount must be greater than 0");
+        }
+        transactionDto.setTransactionDate(new Date(Calendar.getInstance().getTimeInMillis()));
+        Account account = accountService.findById(transactionDto.getAccountId());
+        Transaction transaction = transactionMapper.transactionDtoToTransaction(transactionDto);
+        transaction.setAccountId(account);
+        return transactionMapper.transactionToTransactionDto(transactionRepository.save(transaction));
+    }
+
+    @Override
+    public List<TransactionDto> getAll(long userId) {
+        return userRepository.findById(userId).map(user -> {
             List<Account> accounts= accountRepository.findAllByUser(user);
             List<Transaction> transactions= new ArrayList<>();
             accounts.forEach(acc -> {
