1abaf8dff2c2fc95a2f9968b8665b389283e8f11
William Fonseca <santifon15@gmail.com>
2022-11-07 08:59:01 -0500

    add security context to transaction controller

--- src/main/java/org/alkemy/wallet/controller/TransactionController.java
+++ src/main/java/org/alkemy/wallet/controller/TransactionController.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.alkemy.wallet.dto.TransactionDto;
+import org.alkemy.wallet.exception.UnAuthorizedException;
 import org.alkemy.wallet.model.TypeTransaction;
+import org.alkemy.wallet.repository.IUserRepository;
 import org.alkemy.wallet.service.ITransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class TransactionController {
 
     private final ITransactionService transactionService;
+    private IUserRepository userRepository;
 
     @Autowired
     public TransactionController(ITransactionService transactionService) {
@@ -24,12 +28,19 @@ public class TransactionController {
 
     @GetMapping("/{user_id}")
     public ResponseEntity<List<TransactionDto>> getAllByUser(@PathVariable(name = "user_id") long userId){
+        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        long user = userRepository.findByEmail(userEmail).getId();
+        if(user != userId){
+            throw new UnAuthorizedException("Unable to see other user's transactions");
+        }
         return new ResponseEntity<>(transactionService.getAllByUser(userId), HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<TransactionDto> edit(@PathVariable(name = "id") long id, @RequestBody Map<String, String> requestBody){
-        return new ResponseEntity<TransactionDto>(transactionService.edit(id, id, requestBody.get("description")), HttpStatus.OK);
+        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        long userId = userRepository.findByEmail(userEmail).getId();
+        return new ResponseEntity<TransactionDto>(transactionService.edit(userId, id, requestBody.get("description")), HttpStatus.OK);
     }
 
     @PostMapping("/deposit")
