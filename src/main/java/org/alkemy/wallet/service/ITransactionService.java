556130124e24eb8e316c85052fa3587588760d79
Natalia <gamarranatalia8@gmail.com>
2022-11-07 10:15:36 -0300

    AL-21: Method added in TransactionService for find by id

--- src/main/java/org/alkemy/wallet/service/ITransactionService.java
+++ src/main/java/org/alkemy/wallet/service/ITransactionService.java
@@ -1,10 +1,11 @@
 package org.alkemy.wallet.service;
 
 import org.alkemy.wallet.dto.TransactionDto;
-import org.alkemy.wallet.model.Transaction;
 
 public interface ITransactionService {
 
     public TransactionDto save(TransactionDto transaction);
+
+   TransactionDto findById(Long id);
 
 }
