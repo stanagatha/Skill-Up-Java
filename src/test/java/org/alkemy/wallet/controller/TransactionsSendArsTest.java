0f9ef85c31038b676809437ec3a8aa10053e2933
julianDGS <jdjuliangs@gmail.com>
2022-11-09 18:34:33 -0500

    AL-36 test with token provided

--- src/test/java/org/alkemy/wallet/controller/TransactionsSendArsTest.java
+++ src/test/java/org/alkemy/wallet/controller/TransactionsSendArsTest.java
@@ -8,10 +8,12 @@ import org.alkemy.wallet.repository.ITransactionRepository;
 import org.alkemy.wallet.repository.IUserRepository;
 import org.alkemy.wallet.security.JwtTokenUtil;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +23,9 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -85,5 +90,18 @@ public class TransactionsSendArsTest {
         transactionSendMoneyDto.setDestinationAccountId(2L);
 
         jsonMapper = new ObjectMapper();
+    }
+
+    @Test
+    void postSendArs_TokenProvided_OkResponse() throws Exception {
+        mockMvc.perform(post("/transactions/sendArs")
+                        .header("Authorization", "Bearer " + userToken)
+                        .contentType(MediaType.APPLICATION_JSON).content(jsonMapper.writeValueAsString(transactionSendMoneyDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.amount").value(550D))
+                .andExpect(jsonPath("$.accountId").value(1L))
+                .andExpect(jsonPath("$.description").value("SendArs"))
+                .andExpect(jsonPath("$.typeTransaction").value(TypeTransaction.PAYMENT
+                        .name()));
     }
 }
