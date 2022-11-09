c9fbf9adadf1c3bc92d55053e8c5f39d4aeed785
Natalia <gamarranatalia8@gmail.com>
2022-11-09 19:00:42 -0300

    AL-25:  Added UserUpdateDto

--- src/main/java/org/alkemy/wallet/dto/UserUpdateDto.java
+++ src/main/java/org/alkemy/wallet/dto/UserUpdateDto.java
@@ -1,2 +1,9 @@
-package org.alkemy.wallet.dto;public class UserUpdateDto {
+package org.alkemy.wallet.dto;
+
+import java.io.Serializable;
+
+public class UserUpdateDto implements Serializable {
+
+    public String firstName;
+    public String lastName;
 }
