a4cdf3b88903a53100d14586beb20110a7e7e4db
Ignacio Avila <ignacioavilamdp@gmail.com>
2022-11-10 16:39:01 -0300

    AL-54: Modified user controller test to support the paged results.

--- src/test/java/org/alkemy/wallet/controller/UserControllerTest.java
+++ src/test/java/org/alkemy/wallet/controller/UserControllerTest.java
@@ -13,9 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,7 +26,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -44,6 +46,7 @@ class UserControllerTest {
     // Test fixture
     private String user1Token, user2Token, admin1Token;
     private User user1, user2, admin1;
+    private Page<User> usersPage0, usersPage1;
     private List<User> userList;
 
     @BeforeEach
@@ -51,17 +54,13 @@ class UserControllerTest {
         Role userRole = new Role(1L, RoleName.USER, "USER Role", new Date(), new Date());
         Role adminRole = new Role(2L, RoleName.ADMIN, "ADMIN Role", new Date(), new Date());
 
-        user1 = new User("User1FN", "User1LN", "user1Email@email.com", "1234", userRole);
-        user2 = new User("User2FN", "User2LN", "user2Email@email.com", "4321", userRole);
-        admin1 = new User("Admin1FN", "Admin1LN", "admin1Email@email.com", "5678", adminRole);
-        user1.setId(1L);
-        user2.setId(2L);
-        admin1.setId(3L);
+        user1 = new User(1L, "User1FN", "User1LN", "user1Email@email.com", "1234", userRole, new Date(), new Date(), false);
+        user2 = new User(2L, "User2FN", "User2LN", "user2Email@email.com", "4321", userRole, new Date(), new Date(), false);
+        admin1 = new User(3L, "Admin1FN", "Admin1LN", "admin1Email@email.com", "5678", adminRole, new Date(), new Date(), false);
 
-        userList = new ArrayList<>();
-        userList.add(user1);
-        userList.add(user2);
-        userList.add(admin1);
+        userList = List.of(user1, user2, admin1);
+        usersPage0 = new PageImpl<>(userList.subList(0, 2), PageRequest.of(0,2), 3);
+        usersPage1 = new PageImpl<>(userList.subList(2, 3), PageRequest.of(1,2), 3);
 
         UserDetails loggedUser1Details = new org.springframework.security.core.userdetails.User(user1.getEmail(), user1.getPassword(), new ArrayList<>());
         UserDetails loggedUser2Details = new org.springframework.security.core.userdetails.User(user2.getEmail(), user2.getPassword(), new ArrayList<>());
@@ -77,11 +76,14 @@ class UserControllerTest {
         when(userRepositoryMock.findById(user2.getId())).thenReturn(Optional.ofNullable(user2));
         when(userRepositoryMock.findById(admin1.getId())).thenReturn(Optional.ofNullable(admin1));
         when(userRepositoryMock.findAll()).thenReturn(userList);
+        when(userRepositoryMock.findAll(PageRequest.of(0,10))).thenReturn(usersPage0);
+        when(userRepositoryMock.findAll(PageRequest.of(1,10))).thenReturn(usersPage1);
     }
 
     @Test
     void getAll_NoTokenProvided_UnauthorizedResponse() throws Exception {
         mockMvc.perform(get("/users").
+                        param("page", "0").
                         contentType(MediaType.APPLICATION_JSON)).
                 andExpect(status().isUnauthorized());
     }
@@ -90,6 +92,7 @@ class UserControllerTest {
     void getAll_WrongTokenProvided_UnauthorizedResponse() throws Exception {
         mockMvc.perform(get("/users").
                         header("Authorization", "Bearer " + user1Token + "b").
+                        param("page", "0").
                         contentType(MediaType.APPLICATION_JSON)).
                 andExpect(status().isUnauthorized());
     }
@@ -98,15 +101,28 @@ class UserControllerTest {
     void getAll_UserRole_ForbiddenResponse() throws Exception {
         mockMvc.perform(get("/users").
                         header("Authorization", "Bearer " + user1Token).
+                        param("page", "0").
                         contentType(MediaType.APPLICATION_JSON)).
                 andExpect(status().isForbidden());
     }
 
     @Test
-    void getAll_AdminRole_OkResponse() throws Exception {
-        String expectedJson = jsonMapper.writeValueAsString(userList.stream().map(userMapper::userToUserDTO).collect(Collectors.toList()));
+    void getAll_AdminRoleRequestPage0_OkResponse() throws Exception {
+        String expectedJson = jsonMapper.writeValueAsString(usersPage0.map(user -> userMapper.userToUserDTO(user)));
         mockMvc.perform(get("/users").
                         header("Authorization", "Bearer " + admin1Token).
+                        param("page", "0").
+                        contentType(MediaType.APPLICATION_JSON)).
+                andExpect(status().isOk()).
+                andExpect(content().json(expectedJson));
+    }
+
+    @Test
+    void getAll_AdminRoleRequestPage1_OkResponse() throws Exception {
+        String expectedJson = jsonMapper.writeValueAsString(usersPage1.map(user -> userMapper.userToUserDTO(user)));
+        mockMvc.perform(get("/users").
+                        header("Authorization", "Bearer " + admin1Token).
+                        param("page", "1").
                         contentType(MediaType.APPLICATION_JSON)).
                 andExpect(status().isOk()).
                 andExpect(content().json(expectedJson));
