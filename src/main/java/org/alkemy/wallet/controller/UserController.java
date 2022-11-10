c81102a1a4c12121cfecd3e59ab3e4bf767218e2
Alejandro Corvalán <81098953+no7sag@users.noreply.github.com>
2022-11-10 20:54:14 -0300

    AL-31: [hotfix] Add documentation for recently created endpoints

--- src/main/java/org/alkemy/wallet/controller/UserController.java
+++ src/main/java/org/alkemy/wallet/controller/UserController.java
@@ -22,36 +22,38 @@ public class UserController {
     public UserController(IUserService userService) {
         this.userService = userService;
     }
-    
+
     @Operation(summary = "Get all users",
-               description = "Only accessible as an ADMIN.")
+            description = "Only accessible as an ADMIN.")
     @Secured({"ROLE_ADMIN"})
     @GetMapping
     public ResponseEntity<Page<UserDto>> getAll(@RequestParam("page") Integer pageNumber) {
         return ResponseEntity.ok().body(userService.getAll(pageNumber));
     }
-    
+
     @GetMapping("/current")
-    @Operation(summary = "Get information from currently authenticated user")
+    @Operation(summary = "Get information from the currently authenticated user")
     public ResponseEntity<UserDto> getCurrent() {
         return ResponseEntity.ok().body(userService.getCurrent());
     }
 
     @DeleteMapping("/{userId}")
-    @Operation(summary = "Delete a user by providing an user ID",
-               description = "As an ADMIN, can delete any user. As a USER, can only delete themself.")
+    @Operation(summary = "Delete a user",
+            description = "As an ADMIN, can delete any user. As a USER, can only delete themself.")
     public ResponseEntity<String> deleteById(@PathVariable("userId") Long id) {
         return ResponseEntity.ok().body(userService.deleteById(id));
     }
 
-
     @PatchMapping("/{id}")
+    @Operation(summary = "Update a user",
+            description = "Can't modify \"mail\" nor \"role\" fields.")
     public ResponseEntity<UserDto> UpdateUser(@PathVariable("id") Long id,
                                               @RequestBody UserUpdateDto userUpdateDto){
         return ResponseEntity.ok().body(userService.updateUser(id, userUpdateDto));
     }
-        
+
     @GetMapping("/{id}")
+    @Operation(summary = "Get user information from provided ID")
     public ResponseEntity<UserDto> getById(@PathVariable Long id){
         return ResponseEntity.ok().body(userService.getById(id));
     }
