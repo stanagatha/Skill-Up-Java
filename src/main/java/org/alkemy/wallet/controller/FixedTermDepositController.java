529321435de955f93807ea3ae27d1f469d97bab3
Alejandro Corvalán <81098953+no7sag@users.noreply.github.com>
2022-11-11 13:34:34 -0300

    AL-33: Add Transactions documentation

--- src/main/java/org/alkemy/wallet/controller/FixedTermDepositController.java
+++ src/main/java/org/alkemy/wallet/controller/FixedTermDepositController.java
@@ -1,5 +1,7 @@
 package org.alkemy.wallet.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.alkemy.wallet.dto.FixedTermDepositDto;
 import org.alkemy.wallet.dto.FixedTermDepositRequestDto;
 import org.alkemy.wallet.dto.FixedTermDepositSimulateDto;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "Fixed Term Deposits", description = "FixedTermDepositController")
 @RequestMapping("/fixedDeposit")
 public class FixedTermDepositController {
 
@@ -21,11 +24,20 @@ public class FixedTermDepositController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a term deposit",
+            description = "0.5% interest rate per day.<br>" +
+                    "Closing date must be less than 30 days.")
     public ResponseEntity<FixedTermDepositDto> createFixedDeposit(@RequestBody FixedTermDepositRequestDto deposit){
         return ResponseEntity.status(HttpStatus.CREATED).body(fixedTermDepositService.createDeposit(deposit));
     }
+
     @GetMapping("/simulate")
+    @Operation(summary = "Simulate a term deposit",
+            description = "0.5% interest rate per day.<br>" +
+                    "Shows its creation and closing dates, amount invested, interest earned, total amount to collect.<br>" +
+                    "This is a visual simulation and its data won't be stored. ")
     public ResponseEntity<FixedTermDepositSimulateDto> simulateFixedTermDeposit(@RequestBody FixedTermDepositRequestDto depositRequestDto){
         return ResponseEntity.ok().body(fixedTermDepositService.simulateDeposit(depositRequestDto));
     }
+
 }
--- src/main/java/org/alkemy/wallet/controller/TransactionController.java
+++ src/main/java/org/alkemy/wallet/controller/TransactionController.java
@@ -2,6 +2,8 @@ package org.alkemy.wallet.controller;
 
 import java.util.Map;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.alkemy.wallet.dto.TransactionDto;
 import org.alkemy.wallet.dto.TransactionRequestDto;
 import org.alkemy.wallet.dto.TransactionSendMoneyDto;
@@ -16,6 +18,7 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "Transactions", description = "TransactionController")
 @RequestMapping("/transactions")
 public class TransactionController {
 
@@ -26,41 +29,57 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
+    @GetMapping("/{userId}")
+    @Operation(summary = "Get all transactions from the currently authenticated user",
+            description = "Only accessible as an ADMIN. Must provide own user ID.")
     @Secured({"ROLE_ADMIN"})
-    @GetMapping("user/{userId}")
-    public ResponseEntity<Page<TransactionDto>> getAllByUser(@PathVariable(name = "userId") long userId, @RequestParam("page") Integer pageNumber){
+    public ResponseEntity<Page<TransactionDto>> getAllByUser(@PathVariable("userId") long userId,
+                                                             @RequestParam("page") Integer pageNumber){
         return ResponseEntity.ok().body(transactionService.getAllByUser(userId, pageNumber));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a transaction information")
     public ResponseEntity<TransactionDto> getTransactionById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(transactionService.findById(id));
     }
 
     @PatchMapping("/{id}")
+    @Operation(summary = "Edit a transaction information",
+            description = "Can only modify \"descript\" field.")
     public ResponseEntity<TransactionDto> edit(@PathVariable(name = "id") long id,
                                                @RequestBody Map<String, String> requestBody){
         return new ResponseEntity<>(transactionService.edit(id, requestBody.get("description")), HttpStatus.OK);
     }
 
     @PostMapping("/deposit")
+    @Operation(summary = "Make a deposit",
+            description = "Deposit amount must be more than 0.")
     public ResponseEntity<TransactionDto> deposit(@RequestBody TransactionRequestDto transactionDto){
         transactionDto.setTypeTransaction(TypeTransaction.DEPOSIT);
         return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.save(transactionDto));
     }
 
     @PostMapping("/payment")
+    @Operation(summary = "Make a payment",
+            description = "Payment amount must be more than 0.")
     public ResponseEntity<TransactionDto> payment(@RequestBody TransactionRequestDto transactionDto){
         transactionDto.setTypeTransaction(TypeTransaction.PAYMENT);
         return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.save(transactionDto));
     }
 
     @PostMapping("/sendArs")
+    @Operation(summary = "Send money (ARS) to provided account ID",
+            description = "Required information: amount, description, account ID.<br>" +
+                    "Destination account must be from another user.")
     public ResponseEntity<TransactionDto> sendArs(@RequestBody TransactionSendMoneyDto transactionSendMoneyDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.send(transactionSendMoneyDto, Currency.ARS));
     }
 
     @PostMapping("/sendUsd")
+    @Operation(summary = "Send money (USD) to provided account ID",
+            description = "Required information: amount, description, account ID.<br>" +
+                    "Destination account must be from another user.")
     public ResponseEntity<TransactionDto> sendUsd(@RequestBody TransactionSendMoneyDto transactionSendMoneyDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.send(transactionSendMoneyDto, Currency.USD));
     }
